fd72a0aab35a9bb961c4ab5baf9987243fed691e
iuriguerreiro <iurifernandesguerreiro@gmail.com>
2025-05-16 11:17:07 +0200

    articles decoding

--- src/Controllers/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'ArticleController.php';
-
-$articleController = new ArticleController(getPDO());
-
-$articles = $articleController->getAll();
-
-echo($articles[0]['content']);
